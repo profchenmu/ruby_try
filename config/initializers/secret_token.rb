9f6f550f506f25801a010c48d997551763f2d746
lptm <lptm@localhost.localdomain>
2014-03-31 12:58:12 +0800

    test_1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyTry::Application.config.secret_key_base = '8663675188bea7177d2df0800c90c5cd57e1316f81cc9e837538c3920f2dbb92902da7318eab775c9eedab2e146b973dd9a61312b454bc1cfca90c262d256873'
